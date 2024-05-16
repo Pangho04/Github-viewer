7fa274f23fe03f0ad04b56e8e42280949f522134
Pangho04 <dlckdgh741@naver.com>
2024-05-16 23:21:06 +0900

    Feat: try build

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,18 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000 // 크기를 키워 경고를 피할 수 있습니다.
+  },
   test: {
     globals: true,
     environment: "jsdom",
